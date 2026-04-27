bfd5fba3356b4b08b3b38cb010a75062aa056b0f
BMarco77 <beier.marco@web.de>
2026-04-27 21:51:50 +0200

    Update StartScreen.jsx

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -4,19 +4,20 @@ export default function StartScreen({ setMode, setLevel, setScreen, setQuickgues
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#ead0aa] font-lexSerif text-black px-6">
 
-     <div className="text-center mb-6">
+   <div className="text-center mb-3">
   <img
     src={wappen}
     alt="Wappen"
-    className="mx-auto mb-4 max-w-[100px] h-auto"
+    className="mx-auto mb-2 max-w-[90px] h-auto"
   />
 </div>
 
-      <div className="flex items-center justify-center gap-3 my-6">
+<div className="flex items-center justify-center gap-3 mb-5">
   <span className="flex-1 max-w-[150px] h-px bg-black"></span>
   <span className="text-[1.2rem]">❦</span>
   <span className="flex-1 max-w-[150px] h-px bg-black"></span>
 </div>
+
 <h1 className="text-3xl md:text-5xl font-bold mb-10 text-center">
   Typisierungsübungen
 </h1>
