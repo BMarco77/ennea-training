4820bb7410b03b4018d727a499339a6c0218b4f9
BMarco77 <beier.marco@web.de>
2026-04-29 18:13:12 +0200

    Update QuizCard.jsx

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -95,11 +95,7 @@ export default function QuizCard({
         />
 </div>
 
-{mode === "quickguess" && !isTimeUp && (
-  <div className="text-center text-sm mb-1 font-semibold">
-    ⏱ {timer}s
-  </div>
-)}
+
       <div className="text-center font-semibold text-base md:text-lg mb-1.5 leading-tight whitespace-normal break-words">
         {bild.title}
       </div>
